8bc81582027c5a8e49445f5a491ada3679b82872
PranandaNurW <widya.nando.wn@gmail.com>
2024-02-04 18:30:48 +0700

    feat: final project finished

---
+++ js/script.js
@@ -0,0 +1,169 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+// Preloader js
+$(window).on("load", function () {
+  "use strict";
+  $(".preloader").fadeOut(0);
+});
+
+(function ($) {
+  "use strict";
+
+  // tab
+  $(".tab-content")
+    .find(".tab-pane")
+    .each(function (idx, item) {
+      var navTabs = $(this).closest(".code-tabs").find(".nav-tabs"),
+        title = $(this).attr("title");
+      navTabs.append(
+        '<li class="nav-item"><a class="nav-link" href="#">' +
+          title +
+          "</a></li>"
+      );
+    });
+
+  $(".code-tabs ul.nav-tabs").each(function () {
+    $(this).find("li:first").addClass("active");
+  });
+
+  $(".code-tabs .tab-content").each(function () {
+    $(this).find("div:first").addClass("active");
+  });
+
+  $(".nav-tabs a").click(function (e) {
+    e.preventDefault();
+    var tab = $(this).parent(),
+      tabIndex = tab.index(),
+      tabPanel = $(this).closest(".code-tabs"),
+      tabPane = tabPanel.find(".tab-pane").eq(tabIndex);
+    tabPanel.find(".active").removeClass("active");
+    tab.addClass("active");
+    tabPane.addClass("active");
+  });
+
+  // accordion-collapse
+  $(".accordion-collapse").on("show.bs.collapse", function () {
+    $(this).siblings(".accordion-header").addClass("active");
+  });
+  $(".accordion-collapse").on("hide.bs.collapse", function () {
+    $(this).siblings(".accordion-header").removeClass("active");
+  });
+
+  //post slider
+  $(".post-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4500,
+    dots: false,
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="prevArrow"><i class="fas fa-angle-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="nextArrow"><i class="fas fa-angle-right"></i></button>',
+  });
+
+  // videoPopupInit
+  function videoPopupInit() {
+    var $videoSrc;
+    $(".video-play-btn").click(function () {
+      $videoSrc = $(this).data("src");
+    });
+    $("#videoModal").on("shown.bs.modal", function (e) {
+      $("#showVideo").attr(
+        "src",
+        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+      );
+    });
+    $("#videoModal").on("hide.bs.modal", function (e) {
+      $("#showVideo").attr("src", $videoSrc);
+    });
+  }
+  videoPopupInit();
+
+  // table of content
+  new ScrollMenu("#TableOfContents a", {
+    duration: 400,
+    activeOffset: 40,
+    scrollOffset: 10,
+  });
+})(jQuery);
+
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+function createListComponent(taskInputText){
+  // create li
+  const task = document.createElement("li");
+  task.className = "list-group-item border border-light rounded border-1 p-3 mb-2";
+  
+  // create input
+  const taskCheckBox = document.createElement("input");
+  taskCheckBox.type = "checkbox";
+  taskCheckBox.className = "form-check-input strikethrough";
+  
+  // create name
+  const taskName = document.createElement("span");
+  taskName.className = "task-name";
+  taskName.textContent = taskInputText;
+  
+  // create delete button
+  const taskDeleteButton = document.createElement("button");
+  taskDeleteButton.className = "btn btn-sm float-end btn-danger py-1";
+  taskDeleteButton.textContent = "Delete";
+  
+  // add event listener to delete button
+  taskDeleteButton.addEventListener("click", deleteTask);
+
+  // add input, name, button to li. then append li to ul
+  [taskCheckBox, taskName, taskDeleteButton].forEach((item) => task.appendChild(item));
+  taskList.appendChild(task);
+}
+
+function setLocalStorageTasks() {
+  const tasks = [];
+  const taskItem = taskList.getElementsByClassName("task-name"); // element span [span, span, span, span]
+
+  for(let i = 0; i < taskItem.length; i++) {
+    tasks.push(taskItem[i].textContent); // span.textcontent
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  doc = {
+    hobi: ["hoib", "hobo"],
+  }
+}
+
+function getLocalStorageTasks(){
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (tasks) {
+    tasks.forEach(taskText => {
+      createListComponent(taskText);
+    });
+  }
+}
+
+function deleteTask(event) {
+  const task = event.target.parentElement;
+  taskList.removeChild(task);
+
+  setLocalStorageTasks();
+}
+
+function addTask() {
+  const taskInputText = taskInput.value.trim();
+  if (taskInputText !== ""){
+    createListComponent(taskInputText);
+
+    taskInput.value = "";
+
+    setLocalStorageTasks();
+  }
+}
+
+getLocalStorageTasks();
